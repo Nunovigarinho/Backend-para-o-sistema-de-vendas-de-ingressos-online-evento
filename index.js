c222310e0626343053e6446b6114d9ff58e73b89
Nuno Vigarinho <vigarinho146@gmail.com>
2024-10-01 23:26:37 -0300

    Concluindo o back-end.

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import express from "express";
 import rotaevento from "./Rotas/rotaevento.js";
+import cors from "cors";
 
 const app = express();
 const host = "0.0.0.0"; //todas as interfaces de rede
 const porta= 4000;
 
-app.use(express.json()); //o servidor é permitido interpretar o corpo da requisição como JSON
+//o servidor é permitido interpretar o corpo da requisição como JSON
+app.use(cors({
+  origin:"*",
+}));
+
+app.use(express.json()); 
 
 app.use('/eventos', rotaevento);
 
